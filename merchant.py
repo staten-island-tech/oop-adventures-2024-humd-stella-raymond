f6b47d1802995ca697833a9a2092a74160aec42c
stella785 <stellac35@nycstudents.net>
2024-12-12 09:26:16 -0500

    started

---
+++ merchant.py
@@ -0,0 +1,24 @@
+class Merchant:
+    def __init__(self, weapons, potions, armor):
+        self.weapons = weapons
+        self.potions = potions
+        self.armor = armor
+    def sell(self, item):
+        self.products.remove(item)
+        print(f'you have purchased {item}')
+        print(self.products)
+        return item
+    def greeting():
+        print("Welcome!")
+
+weapons = {
+    "Basic Sword": {"damage": 8, "cost": 9},
+    "Silver Sword": {"damage": 10, "cost": 16},
+    "Basic Scythe": {"damage": 14, "cost": 20}
+}
+
+merchant = Merchant(
+    weapons = weapons,
+    potions = ["Healing Potion"],
+    armor = ["Armor"]
+)
