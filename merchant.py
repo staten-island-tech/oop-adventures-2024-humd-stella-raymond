8fc9c59d55853b2d46d6ec0b35d9522d0ed00c10
stella785 <stellac35@nycstudents.net>
2025-01-02 09:20:49 -0500

    started armor class

--- merchant.py
+++ merchant.py
@@ -27,3 +27,13 @@ merchant = Merchant(
     potions = ["Healing Potion"],
     armor = ["Armor"]
 )
+
+class Armor:
+    def __init__(self, defense, damage):
+        self.defense = defense
+        self.damage = damage
+
+    def take_damage(self, damage):
+        reduced_damage = max(0, damage - self.defense)
+        print(f"Armor reduces damage by {self.defense}. Final damage taken: {reduced_damage}")
+        return reduced_damage
