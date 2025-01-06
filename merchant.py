d76a11f973a866fc40a5c472a5e391c9bf37e256
stella785 <stellac35@nycstudents.net>
2025-01-06 09:23:44 -0500

    fixed merchant class

--- merchant.py
+++ merchant.py
@@ -1,15 +1,20 @@
 class Merchant:
-    def __init__(self, weapons, potions, armor):
+    def __init__(self, weapons, potions, armor, products):
         self.weapons = weapons
         self.potions = potions
         self.armor = armor
-    def sell(self, item):
-        self.products.remove(item)
-        print(f'you have purchased {item}')
-        print(self.products)
-        return item
-    def greeting():
-        print("Welcome!")
+        self.products = products
+    def sell(self, item, user_balance):
+        item_cost = item["cost"]
+        if user_balance >= item_cost:
+            user_balance -= item_cost
+            self.products.remove(item)
+            print(f"You have purchased {item} for {item_cost} coins.")
+            print(f"Your remaining balance is {user_balance} coins.")
+            return user_balance
+        else:
+            print("You do not have enough coins to purchase this item.")
+            return user_balance
 
 weapons = {
     "Basic Sword": {"damage": 8, "cost": 9},
@@ -25,7 +30,8 @@ weapons = {
 merchant = Merchant(
     weapons = weapons,
     potions = ["Healing Potion"],
-    armor = ["Armor"]
+    armor = ["Armor"],
+    products = ["products"]
 )
 
 class Armor:
@@ -34,7 +40,7 @@ class Armor:
         self.damage = damage
 
     def take_damage(self, damage):
-        reduced_damage = max(0, damage - self.health)
+        reduced_damage = max(0, self.health - damage)
         print(f"Armor reduces damage by {self.health}. Final damage taken: {reduced_damage}")
         return reduced_damage
 
