f935ddf6e10ec07ec95c42c5e75eadce9083cacd
humd1 <humdb@nycstudents.net>
2025-01-09 23:18:51 -0800

    lots of changes wow

---
+++ pygamething.py
@@ -0,0 +1,17 @@
+import time
+import pygame
+
+pygame.init()  # Initialize all pygame modules
+pygame.mixer.init()  # Initialize mixer
+
+# Load your own music file
+pygame.mixer.music.load("audio-_1_.ogg")  # Replace with the name of your music file
+
+pygame.mixer.music.set_volume(0.4)
+
+# Play the background music (looping indefinitely)
+pygame.mixer.music.play(-1, 0.0)
+
+# Keep the program running while music plays
+while True:
+    time.sleep(1)
