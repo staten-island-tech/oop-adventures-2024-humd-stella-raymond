b238173d712739581900c953a8f7aaef4b8967d5
humd1 <humdb@nycstudents.net>
2025-01-12 11:51:13 -0800

    kiuh

--- pygamething.py
+++ pygamething.py
@@ -9,9 +9,12 @@ pygame.mixer.music.load("audio-_1_.ogg")  # Replace with the name of your music 
 
 pygame.mixer.music.set_volume(0.4)
 
-# Play the background music (looping indefinitely)
+# Play the background music (looping indefinitely, from 0-1.0)
 pygame.mixer.music.play(-1, 0.0)
 
 # Keep the program running while music plays
 while True:
     time.sleep(1)
+
+
+#THANKS CHATGPT
